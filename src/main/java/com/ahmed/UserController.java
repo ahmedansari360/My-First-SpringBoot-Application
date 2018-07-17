2d18105359af2c2599c795ec44df3fe2f4576d85
Muhammad Ahmed Ansari <ahmed.ansari@indusvalleylabs.com>
2018-07-17 11:28:35 +0500

    modified addNewUser() method and added missing HTTP Requests Methods

--- src/main/java/com/ahmed/UserController.java
+++ src/main/java/com/ahmed/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,32 +23,29 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepo;
 
-	@RequestMapping(value = "/addUser", method = RequestMethod.POST, produces = "application/json")
-	public ResponseEntity<?> addNewUser(@RequestParam String name, @RequestParam String username,
-			@RequestParam String password) {
-		User user = new User();
-		user.setName(name);
-		user.setUsername(username);
-		user.setPassword(password);
-		userRepo.save(user);
-		return ResponseEntity.ok(user);
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	public ResponseEntity<?> addNewUser(@RequestBody User user) {
+		User tempUser = new User();
+		tempUser = userRepo.save(user);
+		return ResponseEntity.ok(tempUser);
 	}
 
-	@RequestMapping("/allPopulation")
+	@RequestMapping(value = "/allPopulation", method = RequestMethod.GET)
 	public List<User> getAll() {
 		return userRepo.findAll();
 	}
 
-	@RequestMapping(value = "/showUser/{id}", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/showUser/{id}", method = RequestMethod.GET)
 	public Optional<User> getUser(@PathVariable Integer id) {
 		return userRepo.findById(id);
 	}
 
-	@RequestMapping("/removeUser/{id}")
+	@RequestMapping(value = "/removeUser/{id}", method = RequestMethod.DELETE)
 	public String deleteUser(@PathVariable Integer id) {
 		userRepo.deleteById(id);
 		return "user deleted";
 	}
+
 	@RequestMapping("*")
 	public String fallBack() {
 		return "Bad Request";
